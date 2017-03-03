905da53800eb1dc145fe5f260cc9bc420c7ca686
Aliot <aliot@tut.by>
2017-03-03 16:16:31 +0300

    created new config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,90 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const ProgressPlugin = require('webpack/lib/ProgressPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const HOST = 'localhost';
+const PORT = 8080;
 
-module.exports = {
-    entry: {
-        'app': './src/main.ts'
+const loaders = {
+    componentStyles: {
+        test: /\.css$/,
+        loader: 'style!css-loader'
     },
-    output: {
-        path: './dist',
-        filename: 'app.bundle.js'
+    html: {
+        test: /\.html$/,
+        loader: 'raw-loader'
     },
-    module: {
-        loaders: [
-            {test: /\.ts$/, loader: 'ts'}
-        ]
+    json: {
+        test: /\.json$/,
+        loader: 'json-loader'
     },
-    resolve: {
-        extensions: ['', '.js', '.ts']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+    typescript: {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader?', 'angular2-template-loader'],
+        exclude: /node_modules/
+    }
+};
+
+const config = module.exports = {};
+
+config.entry = {
+    main: ['./src/main.ts'],
+    polyfills: './src/polyfills.ts',
+    vendor: './src/vendor.ts'
+};
+
+config.output = {
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+    publicPath: '/'
+};
+
+config.resolve = {
+    extensions: ['.ts', '.js', '.json', '.css', '.html']
+};
+
+config.module = {
+    loaders: [
+        loaders.typescript,
+        loaders.html,
+        loaders.componentStyles
     ]
-}
+};
+
+config.plugins = [
+    new ContextReplacementPlugin(
+        /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+        path.resolve('src')
+    ),
+    new LoaderOptionsPlugin({
+        options: {
+            tslint: {
+                emitErrors: false,
+                failOnHint: false
+            },
+            postcss: [
+                autoprefixer({
+                    browsers: ['last 2 version']
+                })
+            ]
+        }
+    })
+];
+config.plugins.push(
+    new CommonsChunkPlugin({
+        name: ['vendor', 'polyfills'],
+        minChunks: Infinity
+    }),
+    new HtmlWebpackPlugin({
+        chunkSortMode: 'dependency',
+        filename: 'index.html',
+        hash: false,
+        inject: 'body',
+        template: './src/public/index.html'
+    })
+);
